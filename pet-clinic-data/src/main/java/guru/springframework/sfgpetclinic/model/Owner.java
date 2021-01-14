2c77b4fe9781510e68406b19a8f5a8307e2243be
aOUINA1994 <aouina_@hotmail.com>
2021-01-14 15:44:23 +0100

    Update

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -4,4 +4,7 @@ package guru.springframework.sfgpetclinic.model;
  * Created by jt on 7/13/18.
  */
 public class Owner extends Person {
+    public Long getId(Long id ) {
+        return null;
+    }
 }
