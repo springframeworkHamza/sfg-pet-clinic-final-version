83ed076bd07cd62040db98db91011cb120a1c859
aOUINA1994 <aouina_@hotmail.com>
2021-01-14 15:25:02 +0100

    Implement Services

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/services/OwnerService.java
@@ -0,0 +1,7 @@
+package guru.springframework.sfgpetclinic.model.services;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+
+public interface OwnerService extends CrudService<Owner, Long>{
+    Owner findByLastName(String lastName);
+}
