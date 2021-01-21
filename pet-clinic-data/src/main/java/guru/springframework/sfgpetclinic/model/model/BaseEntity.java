946306ed71f85efd41cc6789af58891460b3c86e
Hamza Laptop <aouina_@hotmail.com>
2021-01-21 13:25:12 +0100

    Updated owner with JPA entity mappings. closes #34

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/model/BaseEntity.java
@@ -9,7 +9,6 @@ import java.io.Serializable;
 @MappedSuperclass
 public class BaseEntity implements Serializable {
 
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
