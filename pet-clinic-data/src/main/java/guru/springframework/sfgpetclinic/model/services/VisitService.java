f64f27423db9884061e66a123827813bd4c4e918
Hamza Laptop <aouina_@hotmail.com>
2021-01-21 16:26:49 +0100

    Create Visit Map Service. closes #42

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/services/VisitService.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfgpetclinic.model.services;
+
+import guru.springframework.sfgpetclinic.model.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
